86d2ceb03ece4983846a83bca79140ae24c60d4c
Robson Vilaça <robsonvilaca.lima@gmail.com>
2022-09-20 12:50:05 -0300

    Criei a classe validator para incluir todos os validadores das telas

---
+++ src/main/java/com/cristal/projetoloja/Validator.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.cristal.projetoloja;
+
+/**
+ *
+ * @author robso
+ */
+public class Validator {
+    
+}
