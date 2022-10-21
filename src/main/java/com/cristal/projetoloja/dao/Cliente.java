9a7e39d8639e8a40108aeeff540d1bb57e7de75a
ROBSON VILACA LIMA <robson.vlima3@senacsp.edu.br>
2022-10-21 19:46:02 -0300

    Subindo classes

---
+++ src/main/java/com/cristal/projetoloja/dao/Cliente.java
@@ -0,0 +1,93 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.cristal.projetoloja.dao;
+
+/**
+ *
+ * @author robson.vlima3
+ */
+public class Cliente {
+
+    private String nome;
+    private String cpf;
+    private String email;
+    private String endereco;
+    private int numeroCasa;
+    private int celular;
+    private int cep;
+    private int dataNasc;
+    
+    
+    public Cliente(){
+    
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public int getNumeroCasa() {
+        return numeroCasa;
+    }
+
+    public void setNumeroCasa(int numeroCasa) {
+        this.numeroCasa = numeroCasa;
+    }
+
+    public int getCelular() {
+        return celular;
+    }
+
+    public void setCelular(int celular) {
+        this.celular = celular;
+    }
+
+    public int getCep() {
+        return cep;
+    }
+
+    public void setCep(int cep) {
+        this.cep = cep;
+    }
+
+    public int getDataNasc() {
+        return dataNasc;
+    }
+
+    public void setDataNasc(int dataNasc) {
+        this.dataNasc = dataNasc;
+    }
+    
+    
+    
+}
---
+++ src/main/java/com/cristal/projetoloja/dao/Produto.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.cristal.projetoloja.dao;
+
+/**
+ *
+ * @author robson.vlima3
+ */
+public class Produto {
+
+    private int codigo;
+    private String descricao;
+    private String cor;
+    private int Tamanho;
+    private double Valor;
+    private int quantidade;
+    private String fornecedor;
+    
+    public Produto(){
+    
+    }
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public int getTamanho() {
+        return Tamanho;
+    }
+
+    public void setTamanho(int Tamanho) {
+        this.Tamanho = Tamanho;
+    }
+
+    public double getValor() {
+        return Valor;
+    }
+
+    public void setValor(double Valor) {
+        this.Valor = Valor;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getFornecedor() {
+        return fornecedor;
+    }
+
+    public void setFornecedor(String fornecedor) {
+        this.fornecedor = fornecedor;
+    }
+    
+}
